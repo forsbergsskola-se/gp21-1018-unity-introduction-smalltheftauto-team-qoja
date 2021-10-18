b52eacbcdcabf1bee1e8a21bd1359a26fab600e1
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-10-18 17:26:30 +0200

    PlayerMovement - JO

--- projects/SmallTheftAuto/Assets/JO/Scripts/PlayerMovementJO.cs
+++ projects/SmallTheftAuto/Assets/JO/Scripts/PlayerMovementJO.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementJO : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class PlayerMovementJO : MonoBehaviour {
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float rotationSpeed = 100.0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.Translate(0f, 0.01f, 0f);
-        }
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0, translation, 0f);
+        transform.Rotate(0, 0, -rotation);
+
     }
 }
+
