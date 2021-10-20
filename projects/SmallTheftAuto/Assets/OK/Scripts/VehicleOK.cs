fb4cb1850bc7b56415a5a2217e2c6e4f06040233
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-10-20 10:23:54 +0200

    Renamed CarControllerOk to VehicleOK

--- projects/SmallTheftAuto/Assets/OK/Scripts/VehicleOK.cs
+++ projects/SmallTheftAuto/Assets/OK/Scripts/VehicleOK.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarControllerOK : MonoBehaviour
+public class VehicleOK : MonoBehaviour
 {
 
     // public GameObject CarPrefab;
