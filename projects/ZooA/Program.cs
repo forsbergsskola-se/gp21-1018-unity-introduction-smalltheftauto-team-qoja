bb6c96123113d541b36150c8e6ca562a05674d68
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-10-20 15:28:27 +0200

    Zoo

---
+++ projects/ZooA/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ZooA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    abstract class Animal
+    {
+        
+    }
+
+    abstract class Mamal : Animal
+    {
+        
+    }
+
+    class Bear : Mamal
+    {
+        
+    }
+
+    class  Lion : Mamal
+    {
+        
+    }
+
+    class Donckey: Mamal
+    {
+        
+    }
+
+    abstract class Fish : Animal
+    {
+        
+    }
+
+    class Salmon: Fish
+    {
+        
+    }
+
+    class Clownfish : Fish
+    {
+        
+    }
+
+   
+
+    class Zoo<T> where T : Animal
+    {
+        
+    }
+
+  
+    
+    
+    
+}
