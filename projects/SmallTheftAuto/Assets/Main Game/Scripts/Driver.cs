efe5c659c4dd708d4f852cf4bd0e4bcb108d3d81
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-10-21 16:13:59 +0200

    Small steps with driver and vehicle

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/Driver.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Driver.cs
@@ -4,37 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour {
-    private GameObject player = GameObject.FindGameObjectWithTag("Player");
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject player;
+    private Vector3 playerOffset = new Vector3(3, 0, 0);
+
+    private bool _isInsideCar;
+
+    void Awake() {
     }
 
-    // Update is called once per frame
     void Update() {
         if (Input.GetButtonDown("Interact-Vehicle")) {
-            //if insidecar
-            //no then you can enter
-            //if yes, you can get out
-            Vehicle[] foundVehicles = FindObjectsOfType<Vehicle>();
-            float[] distancesToVehicles = new float[foundVehicles.Length];
-            
-            for (int i = 0; i < foundVehicles.Length; i++) {
-                distancesToVehicles[i] = Vector2.Distance(this.transform.position, foundVehicles[i].transform.position);
+            if (!_isInsideCar) {
+                EnterClosestVehicle();
+                Debug.Log("I entered!");
+                _isInsideCar = true;
             }
-            
-            int indexOfClosestCar = FindClosestCar(distancesToVehicles);
-            
-            if (distancesToVehicles[indexOfClosestCar] < 3)
-            {
-                foundVehicles[indexOfClosestCar].GetComponent<Vehicle>().EnterCar(player);
-            }
+        }
 
-            
+        if (Input.GetButtonDown("Interact-Vehicle") && _isInsideCar) {
+            Debug.Log("I try to exit!");
+            GetComponent<Vehicle>().ExitCar(playerOffset, player);
+            Debug.Log("I failed to exit!");
+            _isInsideCar = false;
         }
     }
-    
+
     public int FindClosestCar(float[] distancesToVehicles)
     {
         int indexOfClosestVehicle = 0;
@@ -52,18 +46,18 @@ public class Driver : MonoBehaviour {
         return indexOfClosestVehicle;
     }
     
-    public int Min(float[] array)
-    {
-        int index=0;
-        float tmp = 1000f;
-        for(int i=0; i<array.Length; i++)
-        {
-            if (array[i] < tmp)
-            {
-                tmp = array[i];
-                index = i;
-            }
+    private void EnterClosestVehicle() {
+        Vehicle[] foundVehicles = FindObjectsOfType<Vehicle>();
+        float[] distancesToVehicles = new float[foundVehicles.Length];
+
+        for (int i = 0; i < foundVehicles.Length; i++) {
+            distancesToVehicles[i] = Vector2.Distance(this.transform.position, foundVehicles[i].transform.position);
         }
-        return index;
+
+        int indexOfClosestCar = FindClosestCar(distancesToVehicles);
+
+        if (distancesToVehicles[indexOfClosestCar] < 3) {
+            foundVehicles[indexOfClosestCar].GetComponent<Vehicle>().EnterCar(player);
+        }
     }
 }
