d96b197f61fc2c98f8fa7295856c9e035dbe92a9
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-10-21 11:04:22 +0200

    Zoo

--- projects/ZooA/Program.cs
+++ projects/ZooA/Program.cs
@@ -1,102 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ZooA
-{
-    class Program
-    {
-        static void Main(string[] args)
+public class Program {
+    
+    public class Animal{}
+    public class Mammal : Animal{}
+    public class Bear : Mammal{}
+    public class Donkey : Mammal{}
+    public class Lion : Mammal{}
+    public class Fish : Animal{}
+    public class Salmon : Fish{}
+    public class Clownfish : Fish{}
+    public class Student{}
+
+    static void Main() {
         {
             Zoo<Fish> fishZoo = new Zoo<Fish>();
             fishZoo.AddAnimal(new Fish()); // OKAY
             fishZoo.AddAnimal(new Clownfish()); // OKAY
-            
+        }
+        
+        {
             Zoo<Animal> animalZoo = new Zoo<Animal>();
             animalZoo.AddAnimal(new Fish()); // OKAY
             animalZoo.AddAnimal(new Clownfish()); // OKAY
             animalZoo.AddAnimal(new Lion()); // OKAY
             animalZoo.AddAnimal(new Donkey()); // OKAY
-            
+        }
+        
+        {
             Zoo<Lion> lionZoo = new Zoo<Lion>();
-            animalZoo.AddAnimal(new Lion()); // OKAY
-            animalZoo.AddAnimal(new Lion()); // OKAY
-            animalZoo.AddAnimal(new Lion()); // OKAY
-            
-            //Zoo<Fish> fishZoo = new Zoo<Fish>();
-            //fishZoo.AddAnimal(new Lion()); // ERROR!
-            
-            //Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
-            //salmonZoo.AddAnimal(new Fish()); // ERROR!
-
-
+            lionZoo.AddAnimal(new Lion()); // OKAY
+            lionZoo.AddAnimal(new Lion()); // OKAY
+            lionZoo.AddAnimal(new Lion()); // OKAY
         }
-    }
-
-    class Animal
-    {
-        public override string ToString() => GetType().Name;
-    }
-
-    class Mamal : Animal
-    {
         
-    }
-
-    class Bear : Mamal
-    {
+        // {
+        //     Zoo<Student> studentZoo = new Zoo<Student>(); // ERROR!
+        // }
+        //
+        // {
+        //     Zoo<Fish> fishZoo = new Zoo<Fish>();
+        //     fishZoo.AddAnimal(new Lion()); // ERROR!
+        // }
+        //
+        // {
+        //     Zoo<Animal> animalZoo = new Zoo<Animal>();
+        //     animalZoo.AddAnimal(new Student()); // ERROR!
+        // }
+        //
+        // {
+        //     Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
+        //     salmonZoo.AddAnimal(new Fish()); // ERROR!
+        // }
+        //
+        // {
+        //     Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
+        //     salmonZoo.HasAnimal<Lion>(); // ERROR!
+        // }
         
-    }
-
-    class  Lion : Mamal
-    {
-        
-    }
-
-    class Donkey: Mamal
-    {
-        
-    }
-
-    class Fish : Animal
-    {
-        
-    }
-
-    class Salmon: Fish
-    {
-        
-    }
-
-    class Clownfish : Fish
-    {
-        
-    }
-
-   
-
-    class Zoo<T> where T : Animal
-    {
-        private T[] arrayT = { };
-
-        public void AddAnimal(T tAnimal)
         {
-            Array.Resize(ref arrayT, arrayT.Length+1);
-            arrayT[arrayT.Length-1] = tAnimal;
-        }
-
-        public bool HasAnimal<T_>()
-        {
-            foreach (var item in arrayT)
-            {
-                if (item is T_) return true;
-                
-            }
-
-            return false;
+            Zoo<Fish> fishZoo = new Zoo<Fish>();
+            fishZoo.AddAnimal(new Salmon());
+            fishZoo.AddAnimal(new Salmon());
+            Console.WriteLine("This should be False: "+fishZoo.HasAnimal<Clownfish>());
         }
     }
 
-  
-    
-    
-    
+    class Zoo<TAnimal> where TAnimal : Animal {
+        List<TAnimal> animals = new List<TAnimal>();
+
+        public void AddAnimal(TAnimal animal) {
+            this.animals.Add(animal);
+        }
+
+        public bool HasAnimal<TSpecies>() where TSpecies : TAnimal {
+            // TODO return true/false, depending on whether any TAnimal in animals is of type TSpecies
+        }
+    }
 }
