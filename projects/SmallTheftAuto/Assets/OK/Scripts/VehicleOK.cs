40be077e19793728a55041ca441c29b1b5bf4e67
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-10-22 10:12:32 +0200

    doesnt work fully

--- projects/SmallTheftAuto/Assets/OK/Scripts/VehicleOK.cs
+++ projects/SmallTheftAuto/Assets/OK/Scripts/VehicleOK.cs
@@ -1,89 +1,26 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VehicleOK : MonoBehaviour
 {
-
-    // public GameObject CarPrefab;
-
-    public GameObject player;
-
-    //public CarMovementOK movement;
-    public GameObject car;
-
-    private bool touchingCar = false;
-
-    //private bool insideCar = false;
-    private Vector3 playerOffSet = new Vector3(2,0,0);
-
-    
-    
-
-
-    public Rigidbody rig;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<CarMovementOK>().enabled = false; //To automatically assign the Carmovement script
-        //player = FindObjectOfType<PlayerMovementOK>();
-        //PlayerOK playerOk = FindObjectOfType<PlayerOK>();
-        this.player = player;
-
+    void Start() {
+        GetComponent<VehicleMovement>().enabled = false;
     }
-bool PlayerIsInCar()
-    {
-        return !this.player.activeInHierarchy;
+
+    public void EnterCar(GameObject player) {
+        player.SetActive(false);
+        GetComponent<VehicleMovement>().enabled = true;
+        gameObject.tag = "Player";
     }
-    // private void OnCollisionEnter(Collision other)
-    // {
-    //     touchingCar = true;
-    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //FindObjectsOfType<PlayerMovementOK>
-
-        float distance = Vector3.Distance(player.transform.position, this.transform.position);
-
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            
-               if ( this.player.activeInHierarchy && distance < 3)
-               {
-                   EnterCar();
-               }
-               else
-               {
-                   ExitCar();
-               }
-        }
+    public void ExitCar(Vector3 playerOffset, GameObject player) {
+        player.transform.position = transform.position + playerOffset;
+        Debug.Log("I'm suppose to exit");
+        player.SetActive(true);
+        Debug.Log("I'm exited");
+        gameObject.tag = "Vehicle";
+        GetComponent<VehicleMovement>().enabled = false;
         
-
-        void EnterCar()
-        {
-            player.SetActive(false);
-            GetComponent<CarMovementOK>().enabled = true;
-            this.gameObject.tag = "Player";
-            
-            //insideCar = true;
-           // PlayerIsInCar();
-
-        }
-
-        void ExitCar()
-        {
-            
-            player.SetActive(true);
-            player.transform.position = car.transform.position + playerOffSet;
-            GetComponent<CarMovementOK>().enabled = false;
-            this.gameObject.tag = "Vehicle";
-           // insideCar = false;
-           
-        }
-          
     }
 }
