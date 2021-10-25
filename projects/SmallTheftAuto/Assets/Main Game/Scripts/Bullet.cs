3c3ad70fa34e426ab113ba92befbfa12c8a21388
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-10-25 19:17:57 +0200

    Bullet Script

---
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float bulletSpeed = 100f;
+    public Rigidbody2D bulletRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRb.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
