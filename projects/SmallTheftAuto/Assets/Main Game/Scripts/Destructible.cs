810620f590092b2ee85c1a70f5a5488d777d5a82
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-10-26 12:02:52 +0200

    Destructible script
    Can now set objects on fire from car explosion

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/Destructible.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Destructible.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destructible : MonoBehaviour
+public class Destructible : MonoBehaviour, IBurnable, IDamageable
 {
     [SerializeField] private int health = 100;
     [SerializeField] private int fireThreshold = 30;
-    public void InflictDamage(int damage) {
-        health -= damage;
-    }
 
     public void OnFire() {
-        
+        Debug.Log(gameObject + "on fire");
+        //Trigger fire animation
+        //Fire dies after a certain time
+    }
+
+    public void TakeDamage(int damage) {
+        health -= damage;
     }
 }
