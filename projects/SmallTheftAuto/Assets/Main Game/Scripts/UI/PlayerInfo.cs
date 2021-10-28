9fc4c68579c48acf3e53dbe48f76e8d0dc308612
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-10-28 15:48:28 +0200

    Update PlayerInfo.cs

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/PlayerInfo.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/PlayerInfo.cs
@@ -8,18 +8,24 @@ public class PlayerInfo : MonoBehaviour
     private int money;
     private int score;
     public GameObject player;
+   // public GameManager gameManager;
+    // public GameObject gameManager;
 
-    void Start()
+    void Awake()
     {
         playerInfo = GetComponent<TextMeshProUGUI>();
         playerInfo.enableAutoSizing = true;
+        FindObjectOfType<GameManager>();
+
     }
 
     void Update()
     {
         health = player.GetComponent<Player>().Health;
+
         money = GameManager.instance.Money;
         score = GameManager.instance.Score;
+        //score = GameManager.instance.Score;
         // money = Player.Money;
         // score = player.GetComponent<Player>().Score;
         playerInfo.text = health +"<br><br>"+money+"<br><br>"+score;
