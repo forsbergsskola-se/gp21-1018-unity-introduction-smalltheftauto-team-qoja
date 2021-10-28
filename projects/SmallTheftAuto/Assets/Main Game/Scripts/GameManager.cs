e331942e15f865fbbd1e6df2e7ebbdb7e37b7cd6
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-10-28 10:03:17 +0200

    GameManager file created

---
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+   
+    
+    //Load function
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("MainGameScene"); 
+    }
+    //Restart function
+    //save function
+}
