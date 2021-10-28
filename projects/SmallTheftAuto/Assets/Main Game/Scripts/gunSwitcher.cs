3f000a6374105f4c2e17697a7d80a0a656348692
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-10-28 08:52:20 +0200

    gunSwitcher script updated

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/gunSwitcher.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/gunSwitcher.cs
@@ -1,16 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class gunSwitcher : MonoBehaviour
 {
+    int totalWeapons = 1;
+    public int currentWeaponIndex;
     
+    public GameObject[] guns;
+    public GameObject GunHolder;
+    public GameObject currentGun;
+    
+    void Start()
+    {
+        totalWeapons = GunHolder.transform.childCount;
+        guns = new GameObject[totalWeapons];
+
+        for (int i = 0; i < totalWeapons; i++)
+        {
+            guns[i] = GunHolder.transform.GetChild(i).gameObject;
+            guns[i].SetActive(false);
+        }
+        guns[0].SetActive(true);
+        currentGun = guns[0];
+        currentWeaponIndex = 0;
+
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        // Next Weapon
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            SwitchGuns();
+            if (currentWeaponIndex < totalWeapons-1)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex += 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            
         }
+        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (currentWeaponIndex > 0)
+            {
+                guns[currentWeaponIndex].SetActive(false);
+                currentWeaponIndex -= 1;
+                guns[currentWeaponIndex].SetActive(true);
+            }
+            
+        }
+        
+        
+        //if (Input.GetKeyDown(KeyCode.Tab))
+        //{
+            //SwitchGuns();
+        //}
     }
 
     void SwitchGuns()
