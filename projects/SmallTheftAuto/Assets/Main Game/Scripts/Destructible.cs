f6e874318e3d6c0cb77eaeb8f897872e431e3e24
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-10-28 17:46:21 +0200

    Health works and clamps correctly

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/Destructible.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Destructible.cs
@@ -15,8 +15,7 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
     private Player player;
     private Building building;
     private IHaveHealth healthInterface;
-    private int health;
-    
+
     //Fire
     public GameObject firePrefab;
     private Vector3 fireOffset = new Vector3(0, 3, 0);
@@ -30,6 +29,7 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
     {
         player = GetComponent<Player>();
         building = GetComponent<Building>();
+        healthInterface = GetComponent<IHaveHealth>();
         HasHealth();
     }
 
@@ -37,12 +37,12 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
     {
         //Update so even if the object don't have health it can be set on fire
 
-        if (health <= 0 && !hasBeenDestroyed) {
+        if (healthInterface.Health <= 0 && !hasBeenDestroyed) {
             OnDeath();
             return;
         }
 
-        if (health <= fireThreshold) {
+        if (healthInterface.Health <= fireThreshold) {
             if (!isBurning && !hasBurned)
             {
                 OnFire(); 
@@ -51,9 +51,7 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
     }
 
     private bool HasHealth() {
-        healthInterface = GetComponent<IHaveHealth>();
         if (healthInterface != null) {
-            health = healthInterface.Health;
             return true;
         }
 
@@ -116,8 +114,8 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
     public void TakeDamage(int damage)
     {
         Debug.Log($"TakeDamage is called on {gameObject} for {damage} damage");
-        health -= damage;
-        Debug.Log($"health of {gameObject} is now {health}");
+        healthInterface.Health -= damage;
+        Debug.Log($"health of {gameObject} is now {healthInterface.Health}");
         
     }
 
@@ -145,7 +143,7 @@ public class Destructible : MonoBehaviour, IBurnable, IDamageable
             Player playerIsInCar = GetComponentInChildren<Player>();
             if (playerIsInCar != null)
             {
-                health = 0;
+                healthInterface.Health = 0;
             }
         }
         
