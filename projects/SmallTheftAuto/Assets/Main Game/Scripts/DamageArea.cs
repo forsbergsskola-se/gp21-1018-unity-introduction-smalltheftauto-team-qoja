e3310db1ca903198eef28f867915207440750ec2
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-10-29 11:54:27 +0200

    Foundation for a damage area
    Doing this so we hopefully will not need to check for tags

---
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/DamageArea.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageArea : MonoBehaviour
+{
+    /*Here damagearea info should go and have a swtich state
+     Maybe it would be possible to do a dropdown list on the ugui to choose which kind of
+     damagearea it is*/
+}
