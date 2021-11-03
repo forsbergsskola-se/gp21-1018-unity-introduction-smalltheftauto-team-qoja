205733b99bc94dc84f899c5ba5d91ac129d7e667
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-11-03 16:14:18 +0100

    StartScreen script
    Will be updated when we have the final game scene

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/StartScreen.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/StartScreen.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //Add scenes to the queue when we have final game
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
