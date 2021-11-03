37d3b688f008ed559330a8d21005e732ca1d41db
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-11-03 17:19:03 +0100

    Removed unneccessary comment I made previously

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/StartScreen.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/StartScreen.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
-    //Add scenes to the queue when we have final game
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
