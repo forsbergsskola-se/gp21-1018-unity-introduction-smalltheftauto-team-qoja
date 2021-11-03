cbae9314ae5678e333441b17df3fb88a955841f5
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-11-03 21:45:27 +0100

    StaminaBar Script

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/StaminaBar.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/StaminaBar.cs
@@ -1,21 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StaminaBar : MonoBehaviour
 {
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
