a45fb18843a5559cf16cd274ddf6998a2c306a92
qiqiapple <qiqiapple@gmail.com>
2021-11-04 11:12:06 +0100

    Parkingspot code doen't run until start the quest. Clean up a bit in instructionUI

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/InstructionUI.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/UI/InstructionUI.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class InstructionUI : MonoBehaviour
 {
-    public GameObject instructionUI;
     public GameObject pauseMenuUI;
-    bool instructionIsOn = false;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (instructionIsOn)
-            {
-                pauseMenuUI.GetComponent<PauseMenu>().Resume();
-            }
+            pauseMenuUI.GetComponent<PauseMenu>().Resume();
         }
     }
        
