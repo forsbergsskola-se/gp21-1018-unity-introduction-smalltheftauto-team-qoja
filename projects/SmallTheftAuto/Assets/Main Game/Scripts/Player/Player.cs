902c3b6cc44a1e68e67e8d6187b80ddf3a9c6bd3
Armin Afazeli <armin.afazeli@student.forsbergsskola.se>
2021-11-05 09:26:09 +0100

    HealthFinder method removed from player script

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/Player/Player.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Player/Player.cs
@@ -31,10 +31,10 @@ public class Player : MonoBehaviour, IHaveHealth
     private void Update()
     {
         QuestFinder();
-        HealthFinder();
+        
     }
 
-    //Following two methods should be moved out of playerscript
+    //Following method should be moved out of playerscript
     private void QuestFinder()
     {
         if (Input.GetKeyDown(KeyCode.E) && !QuestIsActive)
@@ -58,30 +58,5 @@ public class Player : MonoBehaviour, IHaveHealth
         }
     }
     
-    private void HealthFinder()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            FirstAidKit[] firstAidKits = FindObjectsOfType<FirstAidKit>();
-            
-            if (firstAidKits.Length != 0)
-            {
-                float[] distances = new float[firstAidKits.Length];
-                
-                for (int i = 0; i < firstAidKits.Length; i++)
-                {
-                    distances[i] = Vector2.Distance(this.transform.position, firstAidKits[i].transform.position);
-                }
-                
-                int index = FindObject.FindIndexOfClosestObject(distances);
-                
-                if (distances[index] < 3)
-                {
-                    _firstAidKit = firstAidKits[index].gameObject;
-                    _firstAidKit.SetActive(false);
-                    Health += 10;
-                }
-            }
-        }
-    }
+    
 }
