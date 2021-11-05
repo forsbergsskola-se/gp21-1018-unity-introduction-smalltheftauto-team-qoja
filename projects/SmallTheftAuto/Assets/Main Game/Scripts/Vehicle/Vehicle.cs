9e68b451eaff733c9bebf58f35c89023e441340b
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2021-11-05 17:12:30 +0100

    Update Vehicle.cs

--- projects/SmallTheftAuto/Assets/Main Game/Scripts/Vehicle/Vehicle.cs
+++ projects/SmallTheftAuto/Assets/Main Game/Scripts/Vehicle/Vehicle.cs
@@ -67,7 +67,6 @@ public class Vehicle : MonoBehaviour, IHurtOnCrash, IHaveHealth {
         {
             _inWater = false;
         }
-        
     }
 
     public void EnterCar(GameObject player)
